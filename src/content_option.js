96aa3bb67423d216b0bd63f95f7d538bdd82041c
azaidi <zaidialina1@gmail.com>
2021-10-28 12:40:39 -0400

    pushing to heroku

---
+++ src/content_option.js
@@ -0,0 +1,81 @@
+const logotext = "Alina Zaidi";
+const meta = {
+    title: "Alina Zaidi",
+    description: "I’m a Full Stack Web Developer",
+};
+
+const introdata = {
+    title: "Hi! I'm Alina Zaidi",
+    animated: {
+        first: "I'm a Full Stack Web Developer",
+    },
+    your_img_url: "https://scontent.fkhi17-1.fna.fbcdn.net/v/t1.6435-9/250119610_10159790776799282_6642799511353079272_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=730e14&_nc_ohc=oUjvlx09cp4AX8CbPMb&_nc_oc=AQkvyMtTJNL8E2pQjjttC0FmsC7UGbL-b5cq0VsaFLv-wxCBx54F1bnei82XrVLyKHDDxVSRv-ndQiv25UccIKpe&tn=P_in6YNSuoWj7RSr&_nc_ht=scontent.fkhi17-1.fna&oh=0f891f2029ea5479d3c8bdbf0d309d10&oe=619F6FAE",
+};
+
+const dataabout = {
+    title: "About Me",
+    aboutme: " Hi, I'm Alina! A well-rounded technology professional with a variety of skills related to IT Support, Customer Relations and Programming. Strong problem-solving skills and positive work ethic, with the ability to collaborate and work cross-departmentally.",
+};
+
+
+const skills = [
+
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "CSS",
+        value: 85,
+    },
+];
+
+
+const dataportfolio = [{
+        img: "https://scontent.fkhi17-1.fna.fbcdn.net/v/t1.6435-9/248913029_10159790798939282_5527385772041272322_n.jpg?_nc_cat=106&_nc_rgb565=1&ccb=1-5&_nc_sid=730e14&_nc_ohc=-MEh7BRsjrcAX81KeT1&tn=P_in6YNSuoWj7RSr&_nc_ht=scontent.fkhi17-1.fna&oh=6d203afc8458e4bdfebbdc64ffc96086&oe=61A0A576",
+        desctiption: "FitList App",
+        link: "https://github.com/az84/FitList",
+    },
+    {
+        img: "https://scontent.fkhi17-1.fna.fbcdn.net/v/t1.6435-9/249009136_10159790799059282_1168103520267805199_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=730e14&_nc_ohc=VqjWX6ZSStYAX9pBxXr&_nc_ht=scontent.fkhi17-1.fna&oh=f4f30ca5b9d7ddbd477ebbbf53f63ad8&oe=619F4459",
+        desctiption: "Star Books",
+        link: "https://github.com/az84/Star-Books",
+    },
+    {
+        img: "https://scontent.fkhi17-1.fna.fbcdn.net/v/t1.6435-9/250462522_10159790798944282_5330028782477722391_n.jpg?_nc_cat=110&_nc_rgb565=1&ccb=1-5&_nc_sid=730e14&_nc_ohc=tb9OoEydY_IAX-tIR6e&tn=P_in6YNSuoWj7RSr&_nc_ht=scontent.fkhi17-1.fna&oh=13655cbc75819f41ded3fd7584677867&oe=619ECE6A",
+        desctiption: "Work Day Scheduler",
+        link: "https://github.com/az84/Work-Day-Scheduler",
+    },
+    {
+        img: "https://scontent.fkhi17-1.fna.fbcdn.net/v/t1.6435-9/248919978_10159790798904282_1518242406228333658_n.jpg?_nc_cat=100&_nc_rgb565=1&ccb=1-5&_nc_sid=730e14&_nc_ohc=BEBymExrWcMAX8K408v&_nc_ht=scontent.fkhi17-1.fna&oh=c001d5b1a36a8dab99c2ad68aa545cea&oe=61A0573A",
+        desctiption: "Code Quiz.",
+        link: "https://github.com/az84/Code-Quiz",
+    },
+
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "zaidialina1@gmail.com",
+    // YOUR_SERVICE_ID: "service_id",
+    // YOUR_TEMPLATE_ID: "template_id",
+    // YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/az84",
+    linkedin: "https://www.linkedin.com/in/alinazaidi1098/",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    skills,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
