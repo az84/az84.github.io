80c0236381e85ae3964116c664933a20ab785353
azaidi <zaidialina1@gmail.com>
2021-11-27 17:32:59 -0500

    updates

--- src/content_option.js
+++ src/content_option.js
@@ -9,7 +9,7 @@ const introdata = {
     animated: {
         first: "A Full Stack Web Developer",
     },
-    your_img_url: "https://scontent.fkhi17-1.fna.fbcdn.net/v/t1.6435-9/250119610_10159790776799282_6642799511353079272_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=730e14&_nc_ohc=oUjvlx09cp4AX8CbPMb&_nc_oc=AQkvyMtTJNL8E2pQjjttC0FmsC7UGbL-b5cq0VsaFLv-wxCBx54F1bnei82XrVLyKHDDxVSRv-ndQiv25UccIKpe&tn=P_in6YNSuoWj7RSr&_nc_ht=scontent.fkhi17-1.fna&oh=0f891f2029ea5479d3c8bdbf0d309d10&oe=619F6FAE",
+    your_img_url: "https://scontent-lga3-1.xx.fbcdn.net/v/t39.30808-6/249934910_10159790776779282_1133287564621119700_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=730e14&_nc_ohc=ItK-TvWxL-0AX-HDl6P&_nc_ht=scontent-lga3-1.xx&oh=abf012db191d6adce68e991c66607dca&oe=61A83A62",
 };
 
 const dataabout = {
@@ -20,24 +20,25 @@ const dataabout = {
 
 
 const dataportfolio = [{
-        img: "https://scontent-lga3-1.xx.fbcdn.net/v/t39.30808-6/257288630_10159826240674282_3099098630679471130_n.jpg?_nc_cat=103&_nc_rgb565=1&ccb=1-5&_nc_sid=730e14&_nc_ohc=p_Qz1B3bHagAX-aT9Py&_nc_ht=scontent-lga3-1.xx&oh=54f5442e2beb831af82e902bb1876150&oe=619EEA70",
+        img: "https://scontent-lga3-1.xx.fbcdn.net/v/t39.30808-6/257288630_10159826240674282_3099098630679471130_n.jpg?_nc_cat=103&_nc_rgb565=1&ccb=1-5&_nc_sid=730e14&_nc_ohc=t4LpePqojoYAX81yTjz&_nc_ht=scontent-lga3-1.xx&oh=042008213a538dc44fcfcd27342db955&oe=61A6D370",
         desctiption: "Felicity",
         link: "https://github.com/az84/Felicity-",
     },
-    {
-        img: "https://scontent-lga3-1.xx.fbcdn.net/v/t39.30808-6/257853438_10159831196824282_8093654083244241585_n.jpg?_nc_cat=108&_nc_rgb565=1&ccb=1-5&_nc_sid=730e14&_nc_ohc=13kJEkr-KnIAX8aAd_1&tn=P_in6YNSuoWj7RSr&_nc_ht=scontent-lga3-1.xx&oh=cbf509bfae66895a754804e560938f14&oe=619E4756",
-        desctiption: "Workout Tracker",
-        link: "https://github.com/az84/Workout-Tracker",
-    },
+
     {
         img: "https://raw.githubusercontent.com/az84/Book-Search-Engine/main/Assets/21-mern-homework-demo-01.gif",
         desctiption: "Book Search Engine",
         link: "https://github.com/az84/Book-Search-Engine",
     },
 
+    {
+        img: "https://scontent-lga3-1.xx.fbcdn.net/v/t39.30808-6/257853438_10159831196824282_8093654083244241585_n.jpg?_nc_cat=108&_nc_rgb565=1&ccb=1-5&_nc_sid=730e14&_nc_ohc=2bOoiGc--_4AX_KaYha&tn=P_in6YNSuoWj7RSr&_nc_ht=scontent-lga3-1.xx&oh=78c8c183a79d8f8df9a9c0f7d74e4ab9&oe=61A82A96",
+        desctiption: "Workout Tracker",
+        link: "https://github.com/az84/Workout-Tracker",
+    },
 
     {
-        img: "https://scontent.fkhi17-1.fna.fbcdn.net/v/t1.6435-9/248919978_10159790798904282_1518242406228333658_n.jpg?_nc_cat=100&_nc_rgb565=1&ccb=1-5&_nc_sid=730e14&_nc_ohc=BEBymExrWcMAX8K408v&_nc_ht=scontent.fkhi17-1.fna&oh=c001d5b1a36a8dab99c2ad68aa545cea&oe=61A0573A",
+        img: "https://scontent-lga3-1.xx.fbcdn.net/v/t39.30808-6/250636785_10159790798834282_2524900946112356471_n.jpg?_nc_cat=111&_nc_rgb565=1&ccb=1-5&_nc_sid=730e14&_nc_ohc=0XSkQp9EL_QAX_fgGBs&_nc_ht=scontent-lga3-1.xx&oh=dd9d5de881d27e05e74b29a74aa8b395&oe=61A7E176",
         desctiption: "Code Quiz.",
         link: "https://github.com/az84/Code-Quiz",
     },
