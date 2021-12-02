b7a0bbe900dccd1737e7ff538cd33acc7017be8d
azaidi <zaidialina1@gmail.com>
2021-12-02 18:51:42 -0500

    updates

--- src/content_option.js
+++ src/content_option.js
@@ -9,7 +9,7 @@ const introdata = {
     animated: {
         first: "A Full Stack Web Developer",
     },
-    your_img_url: "https://scontent-lga3-1.xx.fbcdn.net/v/t39.30808-6/262088288_10159858240839282_2958777446522724666_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=730e14&_nc_ohc=5k-ky-DMXEUAX-SUBVX&_nc_ht=scontent-lga3-1.xx&oh=cdcbf1eadd3d6adf7d1d01bb9c0b0d2a&oe=61A9037A",
+    your_img_url: "https://scontent-lga3-1.xx.fbcdn.net/v/t39.30808-6/262088288_10159858240839282_2958777446522724666_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=730e14&_nc_ohc=H5qqMbVP5cEAX8_x_8W&_nc_ht=scontent-lga3-1.xx&oh=243cde7aee07d3979246c4b37b243695&oe=61AEF23A",
 };
 
 const dataabout = {
@@ -20,7 +20,7 @@ const dataabout = {
 
 
 const dataportfolio = [{
-        img: "https://scontent-lga3-1.xx.fbcdn.net/v/t39.30808-6/257288630_10159826240674282_3099098630679471130_n.jpg?_nc_cat=103&_nc_rgb565=1&ccb=1-5&_nc_sid=730e14&_nc_ohc=t4LpePqojoYAX81yTjz&_nc_ht=scontent-lga3-1.xx&oh=042008213a538dc44fcfcd27342db955&oe=61A6D370",
+        img: "https://scontent-lga3-1.xx.fbcdn.net/v/t39.30808-6/257288630_10159826240674282_3099098630679471130_n.jpg?_nc_cat=103&_nc_rgb565=1&ccb=1-5&_nc_sid=730e14&_nc_ohc=hzsAthNBy9wAX-dBqAS&_nc_ht=scontent-lga3-1.xx&oh=d04de505368c049d0d37adda400c8308&oe=61AEBC70",
         desctiption: "Felicity",
         link: "https://github.com/az84/Felicity-",
     },
@@ -32,7 +32,7 @@ const dataportfolio = [{
     },
 
     {
-        img: "https://scontent-lga3-1.xx.fbcdn.net/v/t39.30808-6/257853438_10159831196824282_8093654083244241585_n.jpg?_nc_cat=108&_nc_rgb565=1&ccb=1-5&_nc_sid=730e14&_nc_ohc=2bOoiGc--_4AX_KaYha&tn=P_in6YNSuoWj7RSr&_nc_ht=scontent-lga3-1.xx&oh=78c8c183a79d8f8df9a9c0f7d74e4ab9&oe=61A82A96",
+        img: "https://scontent-lga3-1.xx.fbcdn.net/v/t39.30808-6/257853438_10159831196824282_8093654083244241585_n.jpg?_nc_cat=108&_nc_rgb565=1&ccb=1-5&_nc_sid=730e14&_nc_ohc=A20mVkngPNQAX9_AcKv&tn=P_in6YNSuoWj7RSr&_nc_ht=scontent-lga3-1.xx&oh=56ea0722ec2789372a6a9bb8df19e0aa&oe=61AE1956",
         desctiption: "Workout Tracker",
         link: "https://github.com/az84/Workout-Tracker",
     },
@@ -44,7 +44,7 @@ const dataportfolio = [{
     },
 
     {
-        img: "https://scontent-lga3-1.xx.fbcdn.net/v/t39.30808-6/250636785_10159790798834282_2524900946112356471_n.jpg?_nc_cat=111&_nc_rgb565=1&ccb=1-5&_nc_sid=730e14&_nc_ohc=0XSkQp9EL_QAX_fgGBs&_nc_ht=scontent-lga3-1.xx&oh=dd9d5de881d27e05e74b29a74aa8b395&oe=61A7E176",
+        img: "https://scontent-lga3-1.xx.fbcdn.net/v/t39.30808-6/250636785_10159790798834282_2524900946112356471_n.jpg?_nc_cat=111&_nc_rgb565=1&ccb=1-5&_nc_sid=730e14&_nc_ohc=SyB0o26xXM4AX-p4rr7&_nc_ht=scontent-lga3-1.xx&oh=ee9e27f06aea44f937b152a468448d3c&oe=61ADD036",
         desctiption: "Code Quiz.",
         link: "https://github.com/az84/Code-Quiz",
     },
