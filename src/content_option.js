42efbe017ddc17864e8f7e0892b1bb8e1d223773
azaidi <zaidialina1@gmail.com>
2021-12-08 20:00:23 -0500

    updates

--- src/content_option.js
+++ src/content_option.js
@@ -9,7 +9,7 @@ const introdata = {
     animated: {
         first: "A Full Stack Web Developer",
     },
-    your_img_url: "https://scontent-lga3-1.xx.fbcdn.net/v/t39.30808-6/262088288_10159858240839282_2958777446522724666_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=730e14&_nc_ohc=H5qqMbVP5cEAX8_x_8W&_nc_ht=scontent-lga3-1.xx&oh=243cde7aee07d3979246c4b37b243695&oe=61AEF23A",
+    your_img_url: "https://raw.githubusercontent.com/az84/My-Personal-Portfolio/main/DSC_0928__01.jpg?token=AUCZP4PHXJSJ7CRHC4T27B3BWFKTW",
 };
 
 const dataabout = {
@@ -20,7 +20,7 @@ const dataabout = {
 
 
 const dataportfolio = [{
-        img: "https://scontent-lga3-1.xx.fbcdn.net/v/t39.30808-6/257288630_10159826240674282_3099098630679471130_n.jpg?_nc_cat=103&_nc_rgb565=1&ccb=1-5&_nc_sid=730e14&_nc_ohc=hzsAthNBy9wAX-dBqAS&_nc_ht=scontent-lga3-1.xx&oh=d04de505368c049d0d37adda400c8308&oe=61AEBC70",
+        img: "https://raw.githubusercontent.com/az84/Felicity-/main/screenshots/Carousel1.jpg",
         desctiption: "Felicity",
         link: "https://github.com/az84/Felicity-",
     },
@@ -32,7 +32,7 @@ const dataportfolio = [{
     },
 
     {
-        img: "https://scontent-lga3-1.xx.fbcdn.net/v/t39.30808-6/257853438_10159831196824282_8093654083244241585_n.jpg?_nc_cat=108&_nc_rgb565=1&ccb=1-5&_nc_sid=730e14&_nc_ohc=A20mVkngPNQAX9_AcKv&tn=P_in6YNSuoWj7RSr&_nc_ht=scontent-lga3-1.xx&oh=56ea0722ec2789372a6a9bb8df19e0aa&oe=61AE1956",
+        img: "https://raw.githubusercontent.com/az84/Workout-Tracker/main/ScreenShot.PNG",
         desctiption: "Workout Tracker",
         link: "https://github.com/az84/Workout-Tracker",
     },
@@ -44,7 +44,7 @@ const dataportfolio = [{
     },
 
     {
-        img: "https://scontent-lga3-1.xx.fbcdn.net/v/t39.30808-6/250636785_10159790798834282_2524900946112356471_n.jpg?_nc_cat=111&_nc_rgb565=1&ccb=1-5&_nc_sid=730e14&_nc_ohc=SyB0o26xXM4AX-p4rr7&_nc_ht=scontent-lga3-1.xx&oh=ee9e27f06aea44f937b152a468448d3c&oe=61ADD036",
+        img: "https://raw.githubusercontent.com/az84/Code-Quiz/master/04-web-apis-homework-demo.gif",
         desctiption: "Code Quiz.",
         link: "https://github.com/az84/Code-Quiz",
     },
